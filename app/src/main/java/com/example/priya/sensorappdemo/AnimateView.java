eca92232b73dde5b71208599181c81d945011ade
priya kumari <priyakumari9519@gmail.com>
2017-09-28 18:21:16 +0530

    Initial commit

---
+++ app/src/main/java/com/example/priya/sensorappdemo/AnimateView.java
@@ -0,0 +1,56 @@
+package com.example.priya.sensorappdemo;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.hardware.SensorEvent;
+import android.view.View;
+
+/**
+ * Created by priya on 28/9/17.
+ */
+
+public class AnimateView extends View {
+    private static final int CIRCLE_RADIUS = 15;
+    private Paint paint;
+    private int x;
+    private int y;
+    private int vWidth;
+    private int vHeight;
+//parameterised  constructor of class
+    public AnimateView(Context context) {
+        super(context);
+        paint = new Paint();
+        paint.setColor(Color.GREEN);
+    }
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        vWidth = w;
+        vHeight = h;
+    }
+    // change condition and set position of object
+    public void onSensorEvent (SensorEvent event) {
+        x = x - (int) event.values[0];
+        y = y + (int) event.values[1];
+        if (x <= 0 + CIRCLE_RADIUS) {
+            x = 0 + CIRCLE_RADIUS;
+        }
+        if (x >= vWidth - CIRCLE_RADIUS) {
+            x = vWidth - CIRCLE_RADIUS;
+        }
+        if (y <= 0 + CIRCLE_RADIUS) {
+            y = 0 + CIRCLE_RADIUS;
+        }
+        if (y >= vHeight - CIRCLE_RADIUS) {
+            y = vHeight - CIRCLE_RADIUS;
+        }
+    }
+    //draw circle
+    @Override
+    protected void onDraw(Canvas canvas) {
+        canvas.drawCircle(x, y, CIRCLE_RADIUS, paint);
+        invalidate();
+    }
+}
---
+++ app/src/main/java/com/example/priya/sensorappdemo/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.priya.sensorappdemo;
+
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+    private SensorManager sensorManager;
+    private Sensor accelerometer;
+    private int height,width;
+    private AnimateView mAnimatedView = null;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        init();
+
+    }
+        private void init() {
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        mAnimatedView = new AnimateView(this);
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        height = displayMetrics.heightPixels/2;
+        width = displayMetrics.widthPixels/2;
+        Paint paint=new Paint();
+        paint.setColor(Color.GREEN);
+        new Canvas().drawCircle(width,height,15,paint);
+        setContentView(mAnimatedView);
+    }
+    //on resume of activity,register sensor listener
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this,accelerometer, SensorManager.SENSOR_DELAY_GAME);
+    }
+    //on pause of activity, unregister sensor listener
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+    @Override
+    public void onAccuracyChanged(Sensor arg0, int arg1) {
+
+    }
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            mAnimatedView.onSensorEvent(event);
+        }
+    }
+
+}
+
